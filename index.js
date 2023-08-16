10da76276c70cc1f3d1d859e02d5fe4e665883c9
Adriane <nunez.ad55@gmail.com>
2023-08-16 12:06:21 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
