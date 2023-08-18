92be3af77d55d372dbeb83d33c7907eae3d3124a
Adriane <nunez.ad55@gmail.com>
2023-08-18 12:40:31 +0800

    update

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ const pool = mysql.createPool({
 // Set up middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin: "https://hettrrms.onrender.com",
-  methods: ["GET", "POST", "PUT"],
+  origin: ["https://hettrrms.onrender.com"],
+  method: ["GET", "POST", "PUT"],
   credentials: true
 }));
 
