67fc139cbf0ed72d7440075f6df9535e5c4b066a
Adriane <nunez.ad55@gmail.com>
2023-08-31 19:41:31 +0800

    update

--- index.js
+++ index.js
@@ -430,24 +430,25 @@ app.put('/api/users/:id/image', upload.single('userImage'), (req, res) => {
     res.status(200).json({ success: true });
   });
 });
-// Handle PUT request for updating user info
 app.put('/api/users/:id', async (req, res) => {
   try {
     const userId = req.params.id;
     const { firstName, lastName, email, phoneNumber, birthdate, age, gender } = req.body;
 
+    
+
     const updateUserQuery = 'UPDATE users SET firstName = ?, lastName = ?, email = ?, phoneNumber = ?, birthdate = ?, age = ?, gender = ? WHERE id = ?';
     const updateUserValues = [firstName, lastName, email, phoneNumber, birthdate, age, gender, userId];
 
-    connection.query(updateUserQuery, updateUserValues, (err, result) => {
-      connection.release();
-    
+    pool.query(updateUserQuery, updateUserValues, (err, result) => {
+      pool.end();
+
       if (err) {
         console.error(err);
         res.status(500).json({ error: 'Error updating user' });
         return;
       }
-    
+
       // Return the updated user information
       res.status(200).json({
         id: userId,
@@ -460,12 +461,12 @@ app.put('/api/users/:id', async (req, res) => {
         gender: gender,
       });
     });
-
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error updating user' });
   }
 });
+
 
 //update user password
 app.put('/api/users/:id/password', (req, res) => {
