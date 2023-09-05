7a2e43ebdeec7307d6d7bf4294cfed536ee83fc9
Adriane <nunez.ad55@gmail.com>
2023-09-05 23:01:31 +0800

    update

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(express.static('public'));
 app.use(express.json());
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads');
+    cb(null, path.join(__dirname, 'uploads')); // Use an absolute path
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
